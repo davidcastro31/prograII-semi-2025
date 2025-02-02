f7eee005ab84c50e8367b2ddd303bbb783dd3b50
davidcastro31 <davidguille31@gmail.com>
2025-02-01 20:05:00 -0600

    Tarea: se agrego las opciones de exponente, porcentaje, raiz, factorial y raiz cubica.

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -3,36 +3,50 @@ package com.example.miprimeraaplicacion;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
-
     TextView tempVal;
     RadioGroup rgb;
     RadioButton opt;
+    EditText num1, num2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.btnCalcular);
+        num1 = findViewById(R.id.txtNum1);
+        num2 = findViewById(R.id.txtNum2);
+        rgb = findViewById(R.id.rgoOpciones);
+
+        rgb.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.optRaiz || checkedId == R.id.optFactorial || checkedId == R.id.optCubica) {
+                    num2.setText("");
+                    num2.setEnabled(false);
+                } else {
+                    num2.setEnabled(true);
+                }
+            }
+        });
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tempVal = findViewById(R.id.txtNum1);
                 double num1 = Double.parseDouble(tempVal.getText().toString());
                 tempVal = findViewById(R.id.txtNum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
+                double num2 = tempVal.isEnabled() ? Double.parseDouble(tempVal.getText().toString()) : 0;
                 double respuesta = 0.0;
 
                 opt = findViewById(R.id.optSuma);
@@ -51,10 +65,36 @@ public class MainActivity extends AppCompatActivity {
                 if (opt.isChecked()) {
                     respuesta = num1 / num2;
                 }
+                opt = findViewById(R.id.optExponente);
+                if (opt.isChecked()) {
+                    respuesta = Math.pow(num1, num2);
+                }
+                opt = findViewById(R.id.optPorcentaje);
+                if (opt.isChecked()) {
+                    respuesta = (num1 * num2) / 100;
+                }
+                opt = findViewById(R.id.optRaiz);
+                if (opt.isChecked()) {
+                    respuesta = Math.sqrt(num1);
+                }
+                opt= findViewById(R.id.optFactorial);
+                if (opt.isChecked()){
+                    respuesta = 1;
+                    for (int i = 2; i <= num1; i++){
+                        respuesta *= i;
+                    }
+                }
+                opt = findViewById(R.id.optCubica);
+                if (opt.isChecked()){
+                    respuesta = Math.cbrt(num1);
+                }
 
                 tempVal = findViewById(R.id.lblRespuesta);
-                tempVal.setText("Respuesta: "+ respuesta);
+                tempVal.setText("Respuesta: " + respuesta);
             }
         });
     }
 }
+
+
+
