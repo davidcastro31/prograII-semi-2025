923a2efb7f5b3b267bd79d522015053e30db6422
davidcastro31 <davidguille31@gmail.com>
2025-02-08 16:36:55 -0600

    Uso de notificaciones con toast

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -58,48 +59,62 @@ public class MainActivity extends AppCompatActivity {
                 double num2 = tempVal.isEnabled() ? Double.parseDouble(tempVal.getText().toString()) : 0;
                 double respuesta = 0.0;
 
+                String msg = "";
+
                 spn = findViewById(R.id.spnOpciones);
                 switch (spn.getSelectedItemPosition()){
                     case 0:
                         respuesta = num1 + num2;
+                        msg = "La suma es: "+ respuesta;
                         break;
                     case 1:
                         respuesta = num1 - num2;
+                        msg = "La resta es: "+ respuesta;
                         break;
                     case 2:
                         respuesta = num1 * num2;
+                        msg = "La multiplicacion es: "+ respuesta;
                         break;
                     case 3:
                         respuesta = num1 / num2;
+                        msg = "La division es: "+ respuesta;
                         break;
                     case  4:
                         respuesta = Math.pow(num1, num2);
+                        msg = "La respuesta es: " + respuesta;
                         break;
                     case 5:
                         respuesta = (num1 * num2) / 100;
+                        msg = "El porcentaje es: "+ respuesta;
                         break;
                     case 6:
                         respuesta = Math.sqrt(num1);
+                        msg = "La raiz cuadrada es: "+ respuesta;
                         break;
                     case  7:
                         respuesta = 1;
                         for (int i = 2; i <= num1; i++){
                             respuesta *= i;
                         }
+                        msg = "El factorial es: "+ respuesta;
                         break;
                     case 8:
                         respuesta = Math.cbrt(num1);
+                        msg = "La raiz cubica es: "+ respuesta;
                         break;
                     case 9:
                         respuesta = num1 % num2;
+                        msg = "El modulo es: "+ respuesta;
                         break;
                     case 10:
                         respuesta = Math.max(num1,num2);
+                        msg = "El numero mayor es: "+ respuesta;
                         break;
                 }
 
                 tempVal = findViewById(R.id.lblRespuesta);
                 tempVal.setText("Respuesta: " + respuesta);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
             }
         });
     }
