07ca43abea72c40c9f408b70fc98890d95ee31b5
davidcastro31 <davidguille31@gmail.com>
2025-03-11 15:06:07 -0600

    Creacion de la rama agendaAmigosSQLite

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -1,79 +1,17 @@
 package com.example.miprimeraaplicacion;
-
-
-import android.graphics.Color;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tempVal;
-    SensorManager sensorManager;
-    Sensor sensor;
-    SensorEventListener sensorEventListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        sensorLuz();
-    }
-    @Override
-    protected void onResume() {
-        iniciar();
-        super.onResume();
-    }
-    @Override
-    protected void onPause() {
-        detener();
-        super.onPause();
-    }
-    private void iniciar(){
-        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
-    }
-    private void detener(){
-        sensorManager.unregisterListener(sensorEventListener);
-    }
-    private void sensorLuz(){
-        tempVal = findViewById(R.id.lblSensorProximidad);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        if( sensor==null ){
-            tempVal.setText("Tu dispositivo, NO tiene el senor de PROXIMIDAD");
-            finish();
-        }
-        sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                double valor = event.values[0];
-                tempVal.setText("Proximidad: " + valor);
-                if (valor <= 4) {
-                    getWindow().getDecorView().setBackgroundColor(Color.BLACK);
-                } else if (valor <= 8) {
-                    getWindow().getDecorView().setBackgroundColor(Color.GRAY);
-                } else {
-                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
-                }
-            }
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-        };
     }
 }
-
 
 
 
